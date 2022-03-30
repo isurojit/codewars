58c70a5ddaba04814a078069cd756a388eac51ce
isurojit <iamsurojit@hotmail.com>
2022-03-30 16:14:08 +0530

    js added

--- theVowelCode/dist/theVowelCode.dev.js
+++ theVowelCode/dist/theVowelCode.dev.js
@@ -1,21 +1,18 @@
-// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
+"use strict";
 
+// Step 1: Create a function called encode() to replace all the lowercase vowels in a given string with numbers according to the following pattern:
 // a -> 1
 // e -> 2
 // i -> 3
 // o -> 4
 // u -> 5
 // For example, encode("hello") would return "h2ll4". There is no need to worry about uppercase vowels in this kata.
-
 // Step 2: Now create a function called decode() to turn the numbers back into vowels according to the same pattern shown above.
-
 // For example, decode("h3 th2r2") would return "hi there".
-
 // For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
-const encode = (string) => {
-  let newStringArray = [];
-
-  string.split("").forEach((ele) => {
+var encode = function encode(string) {
+  var newStringArray = [];
+  string.split("").forEach(function (ele) {
     if (ele.toLowerCase() === "a") {
       newStringArray.push("1");
     } else if (ele.toLowerCase() === "e") {
@@ -32,10 +29,10 @@ const encode = (string) => {
   });
   return newStringArray.join("");
 };
-const decode = (string) => {
-  let newStringArray = [];
 
-  string.split("").forEach((ele) => {
+var decode = function decode(string) {
+  var newStringArray = [];
+  string.split("").forEach(function (ele) {
     if (ele.toLowerCase() === "1") {
       newStringArray.push("a");
     } else if (ele.toLowerCase() === "2") {
