113b75eee83778c75f1766f363704fd7307e8c64
isurojit <iamsurojit@hotmail.com>
2022-05-30 16:49:12 +0530

    added more sol for getSum

--- getSum/getSum,.js
+++ getSum/getSum,.js
@@ -30,3 +30,34 @@ function getSum(a, b) {
 }
 
 console.log(getSum(-1, 2));
+
+//Some Of The Best Solutions
+
+//Sol -1 using max and min
+const GetSum = (a, b) => {
+  let min = Math.min(a, b),
+    max = Math.max(a, b);
+  return ((max - min + 1) * (min + max)) / 2;
+};
+
+//Sol -2 basic if else
+function GetSum(a, b) {
+  if (a == b) return a;
+  else if (a < b) return a + GetSum(a + 1, b);
+  else return a + GetSum(a - 1, b);
+}
+
+//Sol -3 using arithmatic option
+function GetSum(a, b) {
+  return ((Math.abs(a - b) + 1) * (a + b)) / 2;
+}
+
+//Sol -4 using if-while
+function GetSum(a, b) {
+  tmp = 0;
+
+  if (a < b) while (a <= b) tmp += a++;
+  else while (a >= b) tmp += a--;
+
+  return tmp;
+}
